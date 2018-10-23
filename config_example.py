e937ff7232e27058bb97d91d76a0b46c75c6f3cd
Sebastiaan van Wagensveld <sebastiaan@SebastiaannoMacBook-puro.local>
2018-10-23 22:30:12 +1100

    Adding example config

---
+++ config_example.py
@@ -0,0 +1,10 @@
+# Tokens
+DISCORD_TOKEN = ""
+SAUCENAO_TOKEN = ""
+YANDEX_TOKEN = ""
+
+# Embed Resources
+PADORU_ICON = "https://bot.padoru.moe/data/padoru.png"
+PADORU_HEADS = "https://bot.padoru.moe/data/padoru-head.png"
+PADORU_TAILS = "https://bot.padoru.moe/data/padoru-tail.png"
+PADORU_COLOUR = 0xff0000
